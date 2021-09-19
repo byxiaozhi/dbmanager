70c0576a5ed1626e5b2ea97d83b40be7593cd19d
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2021-09-20 04:30:32 +0800

    Update EventEmitter.java

--- src/main/java/com/zzf/dbmanager/utils/EventEmitter.java
+++ src/main/java/com/zzf/dbmanager/utils/EventEmitter.java
@@ -8,24 +8,24 @@ import java.util.function.Consumer;
 
 public class EventEmitter {
 
-    Map<String, List<Consumer<Object>>> listenersMap = new HashMap<>();
+    public static Map<String, List<Consumer<Object>>> listenersMap = new HashMap<>();
 
-    public void addListener(String type, Consumer<Object> listener) {
+    public static void addListener(String type, Consumer<Object> listener) {
         if (!listenersMap.containsKey(type))
             listenersMap.put(type, new ArrayList<>());
         listenersMap.get(type).add(listener);
     }
 
-    public void removeListener(String type) {
+    public static void removeListener(String type) {
         listenersMap.remove(type);
     }
 
-    public void removeListener(String type, Consumer<Object> listener) {
+    public static void removeListener(String type, Consumer<Object> listener) {
         if (listenersMap.containsKey(type))
             listenersMap.get(type).remove(listener);
     }
 
-    public void emit(String type, Object obj) {
+    public static void emit(String type, Object obj) {
         if (listenersMap.containsKey(type))
             listenersMap.get(type).forEach(l -> l.accept(obj));
     }
