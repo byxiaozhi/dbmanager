1f7a296626e14d2656aba7268ffabb54b6f75ca5
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2021-09-20 16:25:24 +0800

    Update Main.java

--- src/main/java/com/zzf/dbmanager/Main.java
+++ src/main/java/com/zzf/dbmanager/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("prism.lcdtext", "false");
         Application.launch(JavaFX.class, args);
     }
 }
